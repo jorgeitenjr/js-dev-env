da15f285f38e0906197083858b1727db9a851c0c
Jorge Luis Iten Júnior <jorgeiten@gmail.com>
2017-11-22 09:42:14 -0200

    webpack setup

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -2,6 +2,6 @@
  * Created by jorgeiten on 21/11/17.
  */
 
-import  chalk from 'chalk';
+import chalk from "chalk";
 
 console.log(chalk.green('Starting app in dev mode...'));
---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+/**
+ * Created by jorgeiten on 22/11/17.
+ */
+import webpack from "webpack";
+import path from "path";
+
+export default {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index.js')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      debug: true,
+      noInfo: false,
+    })
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style-loader', 'css-loader']}
+    ]
+  }
+}
