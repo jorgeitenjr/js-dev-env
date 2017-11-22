b29559393606b23deab436d92abb6de1afd6fe85
Jorge Luis Iten Júnior <jorgeiten@gmail.com>
2017-11-22 11:27:21 -0200

    lint setup

--- src/index.js
+++ src/index.js
@@ -5,5 +5,4 @@ import "./index.css";
 import numeral from "numeral";
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`I would pay  ${courseValue} from this awesome course!`);
+console.log(`I would pay  ${courseValue} from this awesome course!`); //eslint-disable-line no-console
