92a70a6e7a2ec65b881a5cf258252a772cb504c8
Jorge Luis Iten Júnior <jorgeiten@gmail.com>
2017-11-27 12:53:49 -0200

    Testing CI.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /**
  * Created by jorgeiten on 22/11/17.
  */
-import "./index.css";
 import numeral from "numeral";
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay  ${courseValue} from this awesome course!`); //eslint-disable-line no-console
+
